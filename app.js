28db39d349e8e6e43286d49d82aa712e5565e216
Amir Azizov <creator005@mail.ru>
2022-12-25 12:32:03 +0300

    3d items

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const card = document.querySelector('.card');
 const container = document.querySelector('.container')
 
 // Items
-const title = document.querySelector(".title")
-const sneaker = document.querySelector(".sneaker img");
+const title = document.querySelector(".title h1")
+const sneaker = document.querySelector(".sneaker-img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
@@ -19,10 +19,20 @@ container.addEventListener('mousemove', (e) => {
 // Animate In
 container.addEventListener('mouseenter', (e) => {
     card.style.transition = 'none';
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(75px)";
 })
 
 // Animate Out
 container.addEventListener('mouseleave', () => {
     card.style.transition = 'all 0.5s ease';
     card.style.transform = 'rotate(0deg)';
+    title.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
 })
